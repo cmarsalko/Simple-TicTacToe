866ca093674d1f45ac8485edb0e8475686d72d78
cmarsalko <164536750+cmarsalko@users.noreply.github.com>
2024-03-22 12:24:40 -0400

    App.java

--- TicTacToe/src/App.java
+++ TicTacToe/src/App.java
@@ -1,11 +1,4 @@
-/*
- * Created on 2024-02-18
- *
- * Copyright (c) 2024 Nadine von Frankenberg
- */
- 
-// ! NO NEED TO TOUCH THIS FILE !
- public class App {
+public class App {
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.startGame();
